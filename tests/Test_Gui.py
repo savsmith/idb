5e4a79fe528ac0b24633e9b940c488932f85bbf1
kkv263 <kkv263@utexas.edu>
2017-11-26 10:16:26 -0600

    added seach tests for GUI

--- tests/Test_Gui.py
+++ tests/Test_Gui.py
@@ -2,8 +2,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-url = "http://localhost:5000/"
-class PythonOrgSearch(unittest.TestCase):
+url = "https://betterreads.me/"
+class BetterReadsGUI(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -17,6 +17,21 @@ class PythonOrgSearch(unittest.TestCase):
         driver = self.driver
         driver.get(url + 'about')
         self.assertIn("BetterReads", driver.title)
+
+    def test_page_load_search(self):
+        driver = self.driver
+        driver.get(url + 'search')
+        self.assertIn("BetterReads", driver.title)
+
+    def test_search(self): 
+        driver= self.driver
+        driver.get(url + 'search')
+        driver.implicitly_wait(5)
+        element = driver.find_element_by_class_name("searchSearchbar")
+        element.send_keys("harry potter")
+        driver.find_element_by_class_name("searchSearchbutton").click()
+        driver.implicitly_wait(5)
+        self.assertIsNotNone(driver.find_element_by_class_name('resultImage'))
 
     def test_page_load_books(self):
         driver = self.driver
@@ -41,22 +56,27 @@ class PythonOrgSearch(unittest.TestCase):
     def test_books(self):
         driver = self.driver
         driver.get(url + "books")
+        driver.implicitly_wait(5)
         self.assertIsNotNone(driver.find_element_by_class_name('centerCol'))
 
     def test_series(self):
         driver = self.driver
         driver.get(url + "series")
+        driver.implicitly_wait(5)
         self.assertIsNotNone(driver.find_element_by_class_name('centerCol'))
 
     def test_authors(self):
         driver = self.driver
+        driver.implicitly_wait(5)
         driver.get(url + "author")
         self.assertIsNotNone(driver.find_element_by_class_name('centerCol'))
 
     def test_reviews(self):
         driver = self.driver
+        driver.implicitly_wait(5)
         driver.get(url + "reviews")
         self.assertIsNotNone(driver.find_element_by_class_name('centerCol'))
+
 
 
     def tearDown(self):

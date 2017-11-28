ba82892584e1c5db78e698b49dca584e39e0d12c
kkv263 <kkv263@utexas.edu>
2017-11-27 19:55:08 -0600

    upgrade not found page

--- static/js/NotFound.js
+++ static/js/NotFound.js
@@ -4,9 +4,9 @@ require('../css/styles.css');
 var NotFound= React.createClass({
   render: function(){
       return(
-        <div>
-          <h1>Page Not Found</h1>
-          <p>Sorry, but the page you were trying to view does not exist.</p>
+        <div className="notfoundwrapper">
+          <h1 className="notfoundh1">Page Not Found:</h1>
+          <p className="notfoundp">We're sorry! The page you were looking for is not here.</p>
         </div>
       );
   }

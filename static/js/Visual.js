51a30adba83be47b033ba324d197a9fd13272176
kkv263 <kkv263@utexas.edu>
2017-11-27 18:17:25 -0600

    added /visual route

---
+++ static/js/Visual.js
@@ -0,0 +1,14 @@
+import React from 'react';
+require('../css/styles.css');
+
+var Visual = React.createClass({
+  render: function(){
+      return(
+        <div>
+          <h1>test</h1>
+        </div>
+      );
+  }
+});
+  
+  export default Visual; 
